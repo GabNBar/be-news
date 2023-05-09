0f63c7bd284ed274b022496a9df7a87fb8f41b0c
gabnbar <gabitaewang@gmail.com>
2023-05-09 15:51:29 +0100

    3GETapitopics

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,7 +1,6 @@
 const { fetchTopics } = require("../models/topics.model");
 
 exports.getTopics = (req, res, next) => {
-  console.log("in controller");
   return fetchTopics()
     .then((topics) => {
       res.status(200).send({ topics });
--- models/topics.model.js
+++ models/topics.model.js
@@ -1,7 +1,6 @@
 const db = require("../db/connection");
 
 exports.fetchTopics = () => {
-  console.log("in the model");
   return db.query("SELECT * FROM topics;").then((result) => {
     return result.rows;
   });
