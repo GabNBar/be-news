78de11d62d6dd7b13e8db66e156e69d874eb5bba
gabnbar <gabitaewang@gmail.com>
2023-05-12 14:55:23 +0100

    6. GET /api/articles/:article_id/comments

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { getEndpoints } = require("./controllers/endpoints.controller");
 const {
   getArticleById,
   getArticles,
+  getCommentsByArticle,
 } = require("./controllers/articles.controller");
 
 app.get("/api/topics", getTopics);
@@ -15,15 +16,19 @@ app.get("/api", getEndpoints);
 app.get("/api/articles/:article_id", getArticleById);
 //5
 app.get("/api/articles", getArticles);
+//6
+app.get("/api/:article_id/comments", getCommentsByArticle);
 
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     res
       .status(400)
       .send({ msg: "Bad request, no ID provided for the article." });
+  } else if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
   } else if (err.status === 404) {
     res.status(404).send({ msg: "Article not found!" });
-  }
+  } else next;
 });
 
 app.use((err, req, res, next) => {
